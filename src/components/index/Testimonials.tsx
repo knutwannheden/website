d815a90ee746c6209f878c4b06ee09be5c304c70
Nisar Hassan Naqvi <syednisarhassan12@gmail.com>
2020-11-09 15:47:48 +0000

    Replace embedded tweets with self-build tweets.
    Fixes gitpod-io/website#825

--- src/components/index/Testimonials.tsx
+++ src/components/index/Testimonials.tsx
@@ -1,100 +1,123 @@
-import React from 'react'
+// @ts-nocheck
+import React, { useRef, useState } from 'react'
 
 import styled from '@emotion/styled'
-import TweetEmbed from 'react-tweet-embed'
-import { sizes } from '../../styles/variables'
+import { sizes, colors } from '../../styles/variables'
+import Testimonial, { TestimonialProps } from './Testimonial'
+import { testimonials } from '../../contents'
 
 const StyledTestimonials = styled.div`
-    min-height: 60rem;
-    margin: 8rem 0 10rem;
+  min-height: 60rem;
+  margin: 8rem 0 10rem;
 
-    @media(max-width: ${sizes.breakpoints.sm}) {
-        text-align: center;
+  @media (max-width: ${sizes.breakpoints.sm}) {
+    text-align: center;
+  }
+
+  /* ------------------------------------------- */
+  /* ----- Section Testimonials ----- */
+  /* ------------------------------------------- */
+
+  h3 {
+    margin-bottom: 3rem;
+    text-align: center;
+  }
+
+  .tweets {
+    display: flex;
+    transition: all 0.3s;
+
+    &-container {
+      overflow: hidden;
     }
 
-    /* ------------------------------------------- */
-    /* ----- Section Testimonials ----- */
-    /* ------------------------------------------- */
+    & > * {
+      flex: 0 0 32%;
 
-    h3 {
-        margin-bottom: 3rem;
-        text-align: center;
+      &:not(:last-child) {
+        margin-right: 1.333%;
+      }
+    }
+  }
+
+  .dots {
+    display: flex;
+    justify-content: center;
+    padding: 5rem 0;
+  }
+
+  .dot {
+    height: 1.5rem;
+    width: 1.5rem;
+    background: ${colors.offWhite4};
+    border: none;
+    border-radius: 50%;
+    transition: all .5s;
+
+    &:hover,
+    &:focus {
+        background: ${colors.lightGrey};
     }
 
-    .tweets {
-        display: flex;
-        overflow-x: scroll;
-        align-items: center;
-
-        @media(min-width: calc(${sizes.breakpoints.md} + 1px)) {
-            padding-left: calc((100% - ${sizes.grid.maxWidth})/2);
-        }
-
-        & > div {
-            &:first-of-type {
-                @media(max-width: ${sizes.breakpoints.lg}) {
-                    padding-left: 7rem;
-                }
-
-                @media(max-width: ${sizes.breakpoints.md}) {
-                    padding-left: 4rem;
-                }
-
-                @media(max-width: ${sizes.breakpoints.sm}) {
-                    padding-left: 1rem;
-                }
-            }
-        }
+    &:not(:last-child) {
+      margin-right: 1.5rem;
     }
-
-    .tweet {
-        min-width: 400px;
-
-        @media(max-width: 440px) {
-            min-width: 95%;
-        }
-
-        &:not(:last-of-type) {
-            margin-right: 3rem;
-        }
-    }
-
+  }
 `
 
-const twitterOptions = { theme: 'light', dnt: true, cards: 'hidden' }
+const Testimonials: React.SFC<{}> = () => {
+  const [currentCycle, setCurrentCycle] = useState<number>(0);
+  const tweetsRef = useRef<HTMLDivElement>(null)
+  const cycles = Math.ceil(testimonials.length / 3)
 
-const tweets = [
-  '1115274432958930946',
-  '1102215129696010240',
-  '1167463499779338243',
-  '1191710936605831169',
-  '1131239314346729482',
-  '1217728632887611397',
-  '1117695539540365312',
-  '1116152894548582402',
-  '1159698330764611584',
-  '1117141675745402881',
-  '1120015913024139265',
-  '1221093493214310400',
-  '1215700809104740354'
-]
+  const switchTweets = (to: number) => {
+    const tweets = tweetsRef.current
+    const firstTweet = tweets?.firstChild
+    const spacing = getComputedStyle(firstTweet)['margin-right']
+    const transform = firstTweet.offsetWidth * 3 + parseFloat(spacing.substring(0, spacing.length - 2)) * 2.8
 
-const Testimonials: React.SFC<{}> = () => (
-  <StyledTestimonials>
-    <section className="testimonials">
-      <div className="row">
-        <h3>
-          Used by 200,000+ <strong>Developers & Businesses</strong>
-        </h3>
-      </div>
-      <div className="tweets">
-        {tweets.map((t, i) => (
-          <TweetEmbed key={i} className="tweet" id={t} options={twitterOptions} />
-        ))}
-        <TweetEmbed className="tweet" id="1215707492740739072" options={{ ...twitterOptions, conversation: 'none' }} />
-      </div>
-    </section>
-  </StyledTestimonials>
-)
+    if (null !== tweets) {
+      if (to === 0) {
+        tweets.style.transform = 'none'
+      } else {
+        tweets.style.transform = `translateX(-${transform * to}px)`
+      }
+    }
+  }
+
+  return (
+    <StyledTestimonials>
+      <section className="testimonials">
+        <div className="row">
+          <h3>
+            Used by 200,000+ <strong>Developers & Businesses</strong>
+          </h3>
+          <div className="tweets-container">
+            <div className="tweets" ref={tweetsRef}>
+              {testimonials.map((t) => (
+                <Testimonial key={t.name} {...t} />
+              ))}
+            </div>
+          </div>
+          <div className="dots">
+            {[...Array(cycles)].map((d, i) => (
+              <button
+                className="dot"
+                key={`${d}${i}`}
+                onClick={() => {
+                  setCurrentCycle(i)
+                  switchTweets(i)
+                }}
+                style={currentCycle === i ? {background: colors.lightGrey} : {}}
+              >
+                &nbsp;
+              </button>
+            ))}
+          </div>
+        </div>
+      </section>
+    </StyledTestimonials>
+  )
+}
 
 export default Testimonials
--- src/contents/index.tsx
+++ src/contents/index.tsx
@@ -7,6 +7,19 @@ import KubernetesGraphics from '../resources/kubernetes-graphic.svg'
 import IPad from '../resources/ipad.png'
 import FullAutomationGraphics from '../components/features/FullAutomationGraphics'
 import FullAutomationButtons from '../components/features/FullAutomationButtons'
+import BenHalpern from '../resources/ben-halpern.jpg'
+import HughDurkin from '../resources/hugh-durkin.jpg'
+import Mrugesh from '../resources/mrugesh.jpg'
+import DavidRessler from '../resources/david-ressler.jpg'
+import JonathanListerParsons from '../resources/jayfresh.jpg'
+import SvenPeters from '../resources/sven-peters.jpg'
+import Marcel from '../resources/marcel.jpg'
+import Brenden from '../resources/brenden-o-leary.jpg'
+import MikeNikles from '../resources/mike-nikles.jpg'
+import Alejandro from '../resources/alejandro.jpeg'
+import Joonas from '../resources/joonas-lehtinen.png'
+import { TestimonialProps } from '../components/index/Testimonial'
+
 import { Link } from 'gatsby'
 
 export const features: FeatureCardProps[] = [
@@ -97,4 +110,234 @@ export const features: FeatureCardProps[] = [
       </>
     )
   }
+]
+
+export const testimonials: TestimonialProps[] = [
+  {
+    name: 'Ben Halpern',
+    avatar: BenHalpern,
+    role: (
+      <>
+        Creator and Co-founder of{' '}
+        <a href="https://twitter.com/forem" target="_blank">
+          @forem
+        </a>
+      </>
+    ),
+    text: (
+      <>
+        <p>GitPod is incredibly cool.</p>
+        <p>In my opinion, this is a big step in open source software contribution. I'm excited to see where we go from here.</p>
+      </>
+    ),
+    twitterHandle: 'bendhalpern',
+    tweetId: '1115274432958930946'
+  },
+  {
+    name: 'Hugh Durkin',
+    avatar: HughDurkin,
+    role: (
+      <>
+        Platform Ecosystem at 
+        <a href="https://twitter.com/HubSpot" target="_blank">
+          @HubSpot
+        </a>
+      </>
+    ),
+    text: (
+      <>
+        <p>I've been playing around with the "next generation" of Cloud IDEs lately, and <a href="https://twitter.com/gitpodio" target="_blank">@gitpodio</a> is quickly emerging as one of the best 👨‍💻
+        <br/>
+        Awesome <a href="https://twitter.com/github" target="_blank">@github</a> integration, slick Chrome extension, and super fast 🚀
+        https://gitpod.io/index.html
+        </p>
+      </>
+    ),
+    twitterHandle: 'hughdurkin',
+    tweetId: '1102215129696010240'
+  },
+  {
+    name: 'Mrugesh Mohapatra',
+    avatar: Mrugesh,
+    role: (
+      <>
+        Technology & Community,{' '}
+        <a href="https://twitter.com/freeCodeCamp" target="_blank">
+          @freeCodeCamp
+        </a>
+      </>
+    ),
+    text: (
+      <>
+        <p>
+            Ya'all <a href="https://twitter.com/gitpod" target="_blank">@gitpod</a> is freaking amazing! We integrated it into our workflow for working on <a href="https://twitter.com/freeCodeCamp" target="_blank">@freeCodeCamp</a> 's codebase and I am honestly loving it. 🔥🔥!
+        <br/>   
+            Go check it out on our repo https://github.com/freeCodeCamp/f... now!
+        </p>
+      </>
+    ),
+    twitterHandle: 'raisedadead',
+    tweetId: '1167463499779338243'
+  },
+  {
+    name: 'David Ressler',
+    avatar: DavidRessler,
+    role: (
+      <>
+       Engineering Manager {' '}
+        <a href="https://twitter.com/instagram" target="_blank">
+          @instagram
+        </a>
+      </>
+    ),
+    text: (
+      <>
+        <p>I think I’m in love with <a href="https://twitter.com/gitpod" target="_blank">@gitpod</a>. It’s such a powerful tool. I’ve learned so much in the past week just from stumbling across Github repos that interest me and appending “<a href="http://gitpod.io/#" target="_blank">http://gitpod.io/#</a>” to the URL. A+ tool all around.</p>
+      </>
+    ),
+    twitterHandle: 'DavidRessler',
+    tweetId: '1191710936605831169'
+  },
+  {
+    name: 'Jonathan Lister Parsons',
+    avatar: JonathanListerParsons,
+    role: (
+      <>
+        CTO @ PensionBee
+      </>
+    ),
+    text: (
+      <>
+        <p>I'm really enjoying <a href="https://twitter.com/gitpod" target="_blank">@gitpod</a> - for me, it's made full-stack development in the browser (yes, including mobile) a reality</p>
+      </>
+    ),
+    twitterHandle: 'jayfresh',
+    tweetId: '1217728632887611397'
+  },
+  {
+    name: 'Sven Peters',
+    avatar: SvenPeters,
+    role: (
+      <>
+        Dev Advocate at{' '}
+        <a href="https://twitter.com/MongoDB" target="_blank">
+          @MongoDB
+        </a>
+      </>
+    ),
+    text: (
+      <>
+        <p>Huge time saver, less frustrations for devs: Automated ready-to-code dev environments with pre-build workspaces. #gitpod #cloudide
+        </p>
+      </>
+    ),
+    twitterHandle: 'svenpet',
+    tweetId: '1159698330764611584'
+  },
+  {
+    name: 'Marcel van Remmerden',
+    avatar: Marcel,
+    role: (
+      <>
+       Product Design Manager{' '}
+        <a href="https://twitter.com/GitLab" target="_blank">
+          @GitLab
+        </a>
+      </>
+    ),
+    text: (
+      <>
+        <p>
+            Can only agree, opening a GitLab repository in gitpod is a magical experience, thanks for the integration <a href="https://twitter.com/gitpod" target="_blank">@gitpod</a>!
+        </p>
+      </>
+    ),
+    twitterHandle: 'MvRemmerden',
+    tweetId: '1215707492740739072'
+  },
+  {
+    name: 'Brendan O’Leary',
+    avatar: Brenden,
+    role: (
+      <>
+        Dev. Evangelist{' '}
+        <a href="https://twitter.com/GitLab" target="_blank">
+          @GitLab
+        </a>
+      </>
+    ),
+    text: (
+      <>
+        <p>
+            I just discovered the <a href="https://twitter.com/gitlab" target="_blank">@gitlab</a> and <a href="https://twitter.com/gitpod" target="_blank">@gitpod</a> integration is alive on <a href="http://GitLab.com" target="_blank">http://GitLab.com</a> (released in 13.4 behind a feature flag).  And it's AMAZING <a href="https://docs.gitlab.com/ee/integration/gitpod.html" target="_blank">https://docs.gitlab.com/ee/integration....</a>.
+        </p>
+        <p>Now I can code from *anywhere*</p>
+      </>
+    ),
+    twitterHandle: 'olearycrew',
+    tweetId: '1310376245629198341'
+  },
+  {
+    name: 'Mike Nikles',
+    avatar: MikeNikles,
+    role: (
+      <>
+        Software Architact {' '}
+        <a href="https://twitter.com/Google" target="_blank">
+          @Google
+        </a>
+      </>
+    ),
+    text: (
+      <>
+        <p>12 lines of configuration gives me a 1-click development setup, installs dependencies and starts the dev servers for 3 APIs and 1 frontend application. It also ensures the Svelte extension is installed for all team members.
+        </p>
+        <p>
+            How? With <a href="https://twitter.com/gitpod" target="_blank">@gitpod</a>!
+        </p>
+      </>
+    ),
+    twitterHandle: 'mikenikles',
+    tweetId: '1298711944141000710'
+  },
+  {
+    name: 'Alejandro',
+    avatar: Alejandro,
+    role: (
+      <>
+        Senior dev teaching code thru{' '}
+        <a href="https://twitter.com/4GeeksAcademy" target="_blank">
+          @4GeeksAcademy
+        </a>
+      </>
+    ),
+    text: (
+      <>
+        <p>My setup is on <a href="https://twitter.com/gitpod" target="_blank">@gitpod</a>, just like Christopher McCandless, where I go my code goes with me, no setup whatsoever Smiling face with sunglasses 😎 #IntoTheWild</p>
+      </>
+    ),
+    twitterHandle: 'alesanchezr',
+    tweetId: '1247219573767667715'
+  },
+  {
+    name: 'Joonas Lehtinen',
+    avatar: Joonas,
+    role: (
+      <>
+        Co-founder of Vaadin
+      </>
+    ),
+    text: (
+      <>
+        <p>
+            Wow - <a href="https://twitter.com/gitpod" target="_blank">@gitpod</a> is a brilliant way of evaluating a framework without installing anything.
+        </p>
+        <p> 
+            Launch gitpod for <a href="https://twitter.com/vaadin" target="_blank">@vaadin</a> at <a href="https://vaadin.com/start/v14" target="_blank">https://vaadin.com/start/v14</a> ("Online workspace" tab). You'll have app running and will be editing code in 3min from clicking "Launch" button.
+        </p>
+      </>
+    ),
+    twitterHandle: 'joonaslehtinen',
+    tweetId: '1238597160121200640'
+  },
 ]
